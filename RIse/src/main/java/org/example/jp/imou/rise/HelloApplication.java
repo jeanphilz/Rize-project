25d8b8499ad8db7221defc220fa71db6fc2d2fef
Kwassi Jean-Philippe AHOLOU <96391436+jeanphilz@users.noreply.github.com>
2024-05-29 17:00:11 +0000

    Ajout du Signin

--- RIse/src/main/java/org/example/jp/imou/rise/HelloApplication.java
+++ RIse/src/main/java/org/example/jp/imou/rise/HelloApplication.java
@@ -2,17 +2,19 @@ package org.example.jp.imou.rise;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Rise");
+        Parent root =  FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        Scene scene = new Scene(root);
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
         stage.show();
     }
--- RIse/src/main/java/org/example/jp/imou/rise/controller/SplashScreenController.java
+++ RIse/src/main/java/org/example/jp/imou/rise/controller/SplashScreenController.java
@@ -1,4 +1,75 @@
 package org.example.jp.imou.rise.controller;
 
-public class SplashScreenController {
+import javafx.application.Platform;
+import javafx.concurrent.Task;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SplashScreenController implements Initializable {
+    @FXML
+    private AnchorPane AnchorSplash;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        simulateLoading();
+    }
+
+    private void simulateLoading() {
+        Task<Void> loadingTask = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                // Simulate loading tasks
+                Thread.sleep(3000); // Simulate a 3-second loading time
+
+                return null;
+            }
+        };
+
+        loadingTask.setOnSucceeded(event -> {
+            // Loading is complete, proceed to show the main window
+            showMainWindow();
+        });
+
+        // Start the loading task in a separate thread
+        new Thread(loadingTask).start();
+    }
+
+    private void showMainWindow() {
+        Platform.runLater(() -> {
+            try {
+                Parent root = FXMLLoader.load(getClass().getResource("/org/example/jp/imou/rise/login-view.fxml"));
+                Scene scene = new Scene(root);
+                Stage stage = new Stage();
+                stage.initStyle(StageStyle.DECORATED);
+                stage.setTitle("Rise | Login");
+                stage.setScene(scene);
+                stage.setResizable(false);
+                stage.show();
+
+                // Close the splash screen
+                closeSplashScreen();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+
+
+    private void closeSplashScreen() {
+        Stage splashStage = (Stage) AnchorSplash.getScene().getWindow();
+        splashStage.close();
+    }
+
 }
